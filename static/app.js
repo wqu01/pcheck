93b81869c7f42d1ba3a22d8d36366bcbce18634c
Wen Niao Qu <wqu1202@gmail.com>
2016-07-04 12:44:45 -0400

    added addcart to jolse

--- static/app.js
+++ static/app.js
@@ -3,7 +3,12 @@
 angular.module('pcheckApp', [])
 	.controller('SearchController', ['$scope', '$http', function($scope, $http){
 		$scope.search = function(){
-			
+			$http.get("/jolseresults/"+$scope.item_name)
+	    		.then(function(response) {
+	        		$scope.jolseResults = response.data;
+	        		
+	    	});
+
 	    	//make another request for rrs and tk
 	    	$http.get("/tkresults/"+$scope.item_name)
 	    		.then(function successCallback(response) {
@@ -18,12 +23,7 @@ angular.module('pcheckApp', [])
 	        		$scope.rrsResults = response.data;
 	        		
 	    	});
-	    	$http.get("/jolseresults/"+$scope.item_name)
-	    		.then(function(response) {
-	        		$scope.jolseResults = response.data;
-	        		
-	    	});
-
+	    
 	    			
     	};
 
@@ -38,7 +38,6 @@ angular.module('pcheckApp', [])
 	    		.then(function(response) {
 	        		$scope.jolseResults = response.data;
 	        });
-
     	};
     	$scope.selectRRS = function(choosenLink){
     		$http.get("/rrsprice/"+choosenLink)
@@ -58,4 +57,22 @@ angular.module('pcheckApp', [])
             $scope.showCart = !$scope.showCart;
         };
 
-	}]);
+        $scope.jolseTotal = 0;
+        $scope.jolseCartList = [];
+        $scope.addJolse = function(itemList){
+            console.log("added Jolse");
+            console.log(itemList[3].split(" "));
+            var price = Number(itemList[3].split(" ")[1]);
+            //price is the 3rd index
+            $scope.jolseTotal += price;
+            $scope.jolseCartList.push({
+                item_name: itemList[0],
+                item_price: price
+            });
+            $scope.showCart = true;
+        };
+
+	}])
+    .controller('CartController', ['$scope', function($scope){
+            
+    }]);
